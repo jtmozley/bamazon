73311af81217474ffce58ca4ef66c9820b541280
Justin Mozley <jtmozley@gmail.com>
2019-05-26 21:44:43 -0500

    Completed customer view of Bamazon CLI

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,7 @@ connection.connect(function(err) {
 });
 
 function showItems() {
-  query = "SELECT * FROM bamazon.products";
+  var query = "SELECT * FROM bamazon.products";
   connection.query(query, function(err, res) {
     console.table(res);
     qAndA();
@@ -29,19 +29,77 @@ function showItems() {
 }
 
 function qAndA() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "whatToBuy",
-      message:
-        "Please select the ID number of the item you would like to purchase",
-      choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-    },
-    {
-      type: "input",
-      name: "howMany",
-      message: "How many of this item would you like to purchase?"
-    }
-  ])
-  .then(function());
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "whatToBuy",
+        message:
+          "Please select the ID number of the item you would like to purchase",
+        choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+      },
+      {
+        type: "input",
+        name: "howMany",
+        message: "How many of this item would you like to purchase?"
+      }
+    ])
+    .then(function(response) {
+      var query =
+        "SELECT stock_quantity FROM products where id= '" +
+        response.whatToBuy +
+        "'";
+      connection.query(query, function(err, res) {
+        var dbResponse = res[0].stock_quantity;
+        var requested = JSON.parse(response.howMany);
+        if (requested <= dbResponse) {
+          var query =
+            "UPDATE products SET stock_quantity = stock_quantity - " +
+            requested +
+            " WHERE id = '" +
+            response.whatToBuy +
+            "'";
+          connection.query(query, function(err, res) {
+            var query =
+              "SELECT price FROM products where id = '" +
+              response.whatToBuy +
+              "'";
+            connection.query(query, function(err, res) {
+              var calculation = res[0].price * response.howMany;
+              console.log("Your order total is $" + calculation.toFixed(2));
+              quit();
+            });
+          });
+        } else {
+          console.log("Insufficient quantity!");
+          quit();
+        }
+      });
+    });
+}
+
+function quit() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "exitQuestion",
+        message: "Would you like to place another order?",
+        choices: ["Yes", "No"]
+      }
+    ])
+    .then(function(response) {
+      switch (response.exitQuestion) {
+        case "Yes":
+          showItems();
+          break;
+        case "No":
+          console.log("Thank you for shopping");
+          console.log("Goodbye");
+          process.exit();
+          break;
+        default:
+          break;
+      }
+    });
 }
