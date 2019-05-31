6f7fb8c5bcc7b53b1fc1febb626134477c57b65a
Justin Mozley <jtmozley@gmail.com>
2019-05-31 17:58:33 -0500

    Completed the bamazonManager.js file. App complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -19,62 +19,129 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  startPrompt();
 });
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "mainMenu",
-      message: "What would you like to do?",
-      choices: [
-        "View Products for Sale",
-        "View Low Inventory",
-        "Add to Inventory",
-        "Add New Product"
-      ]
-    }
-  ])
-  .then(function(response) {
-    switch (response.mainMenu) {
-      case "View Products for Sale":
-        viewProd();
-        break;
-      case "View Low Inventory":
-        lowInv();
-        break;
-      case "Add to Inventory":
-        addInv();
-        break;
-      case "Add New Product":
-        newProd();
-        break;
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "mainMenu",
+        message: "What would you like to do?",
+        choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product"
+        ]
+      }
+    ])
+    .then(function(response) {
+      switch (response.mainMenu) {
+        case "View Products for Sale":
+          viewProd();
+          break;
+        case "View Low Inventory":
+          lowInv();
+          break;
+        case "Add to Inventory":
+          addInv();
+          break;
+        case "Add New Product":
+          newProd();
+          break;
 
-      default:
-        break;
-    }
-  });
+        default:
+          break;
+      }
+    });
+}
 
 function viewProd() {
   query = "SELECT * FROM bamazon.products";
   connection.query(query, function(err, res) {
     console.table(res);
+    startPrompt();
   });
 }
 
 function lowInv() {
   query = "SELECT * FROM bamazon.products";
+  var lowInv = [];
   connection.query(query, function(err, res) {
-    for (let i = 1; i < res.length; i++) {
+    for (let i = 0; i < res.length; i++) {
       var element = res[i];
       if (element.stock_quantity <= 5) {
-        console.table(element);
+        lowInv.push(element);
       }
     }
+    console.table(lowInv);
+    startPrompt();
   });
 }
 
 function addInv() {
-    
+  query = "SELECT * FROM bamazon.products";
+  connection
+    .query(query, function(err, res) {
+      console.table(res);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "index",
+            message:
+              "Please select an ID for a product you'd like to change inventory count"
+          },
+          {
+              type: "input",
+              name: "toChange",
+              message: "What would you like to change the count to?"
+          }
+        ])
+        .then(function(response) {
+          query = `UPDATE bamazon.products SET stock_quantity = '${response.toChange}' WHERE (id = '${response.index}')`;
+          connection.query(query, function(err, res) {
+            console.log("Inventory successfully changed");
+            startPrompt();
+          });
+        });
+    })
 }
-function newProd(params) {}
+
+function newProd() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "productName",
+        message: "Product Name:"
+      },
+      {
+        type: "input",
+        name: "departmentName",
+        message: "Department Name:"
+      },
+      {
+        type: "input",
+        name: "stock",
+        message: "Stock Quantity:"
+      },
+      {
+        type: "input",
+        name: "price",
+        message: "List Price:"
+      }
+    ])
+    .then(function(response) {
+      query = `INSERT INTO bamazon.products (product_name, department_name, stock_quantity, price) 
+      VALUES ("${response.productName}", "${response.departmentName}", ${
+        response.stock
+      }, ${response.price})`;
+      connection.query(query, function(err, res) {
+        console.log("Product seccessfully added.");
+        startPrompt();
+      });
+    });
+}
